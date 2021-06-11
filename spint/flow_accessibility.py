d80c2c4bf8e1c603e25cd3846b5bd2459ca3f6ed
Lenka Hasova <tk18583@bristol.ac.uk>
2021-06-11 15:59:26 +0100

    initial accessibility function

--- spint/flow_accessibility.py
+++ spint/flow_accessibility.py
@@ -1,46 +1,43 @@
 # coding=utf-8
-# 3. AFED = Accessibility of flow taking existing destinations
+# 3. FlowAccessibility = Accessibility of flow taking existing destinations
 
-import numpy as np
-import pandas as pd
-
-def AFED(flow_df, row_index): # AFAPF
+def AFED(flow_df, row_index, all_destinations=False): # AFAPF
     
-      
-    # We assume that 
-        # the user has created dataset that has all the possible combinations of origins and destinations, to which he merged distances, flow volumes and destination mass
-            # 1st column = Origin ID
-            # 2nd column = Destination ID
-            # 3rd column = Distance 
-            # 4th column = Weight on flow / flow volume
-            # 5th column = Mass on Destinations
-
-            
-     # rename the columns from an input dataset so we can call them later
+    # rename teh columns so we can call them 
     flow_df = flow_df.rename(columns = {flow_df.columns[0]:'origin_ID', 
                                             flow_df.columns[1]:'dest_ID', 
                                             flow_df.columns[2]:'dist', 
                                             flow_df.columns[3]:'weight', 
                                             flow_df.columns[4]:'dest_mass'})
-    
-    # define Origin and Destination for each row 
+    # define O and D for each row the variables
     D = flow_df['dest_ID'][row_index]
     O = flow_df['origin_ID'][row_index]
     
-    # get the list of possible destinations
-    all_dest = (flow_df.query('origin_ID == @O')
+    
+    # get the list of possible destinations 
+    if all_destinations:
+         all_dest = (flow_df.query('origin_ID == @O')
+                ['dest_ID']
+                .unique()
+               )
+    else:
+        all_dest = (flow_df.query('origin_ID == @O')
                 .query('weight > 0')
                 ['dest_ID']
                 .unique()
-               )    
-    
+               )
+
+        
     # Create all destination flows 
     x1 = pd.DataFrame({'D': np.array([D]*len(all_dest), dtype=object), 
                        'dests':all_dest}).merge(flow_df, how='left', left_on=['D','dests'], right_on=['origin_ID','dest_ID'])
     
     # merge with the distances and masses 
     
+    # Delete the flow to origin
+    x1 = x1[~x1.dests.isin(list(O))]    
+
     # calculate the accessibility
-    A = (x1['weight']*x1['dest_mass']).sum()
+    A = (x1['dist']*x1['dest_mass']).sum()
 
     return A
