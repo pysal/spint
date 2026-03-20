96237581693d02efd2c4729ebb7799486d12cbb4
James Gaboardi <jgaboardi@gmail.com>
2026-03-19 21:56:26 -0700

    migrate to pyproject.toml

--- spint/__init__.py
+++ spint/__init__.py
@@ -1,7 +1,11 @@
-__version__ = '1.0.7'
+import contextlib
+from importlib.metadata import PackageNotFoundError, version
 
 from .gravity import Gravity, Production, Attraction, Doubly
 from .utils import CPC, sorensen, srmse
 from .vec_SA import VecMoran as Moran_Vector
 from .dispersion import phi_disp, alpha_disp
 from .flow_accessibility import Accessibility
+
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = version("spint")
